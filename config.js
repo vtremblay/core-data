07d88179b17666bb68457963a6023fe37ec6582b
Vincent Tremblay <vincent.tremblay@pj.ca>
2016-07-05 10:34:24 -0400

    - Routes

--- config.js
+++ config.js
@@ -18,45 +18,7 @@ module.exports = {
   servicePort: 4100,
   extKeyRequired: false,
   'errors': {},
-  "schema": {
-    "get" : {
-      "/": {
-        "_apiInfo":{
-          "l": "Search UDACs",
-          "group": "Basic",
-          "groupMain": true
-        },
-        "type":{
-          "source": ['query.type'],
-          "validation": {
-            "type": "array",
-            "items": {
-              "type": "string"
-            },
-            "minItems": 0
-          }
-        }
-      }
-    },
-    "post" : {
-      "/": {
-        "_apiInfo":{
-          "l": "Add UDAC",
-          "group": "Basic",
-          "groupMain": true
-        },
-        "data": {
-          "required": true,
-          "source": ["body.data"],
-          "validation": {
-            "type": "object",
-            "properties": udac
-          }
-        }
-      }
-    }
-  }
-  // 'schema': merge.recursive(true, headingConfig, marketConfig, udacConfig)
+  'schema': merge.recursive(true, headingConfig, marketConfig, udacConfig)
 };
 
 
